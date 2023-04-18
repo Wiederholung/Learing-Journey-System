fe4ae4484443833c989393c2555c160d35046a0c
wiederholung <wiederholung@outlook.com>
2023-04-18 13:37:47 +0800

    初始化测试类

---
+++ src/test/java/cn/edu/bupt/DBTest.java
@@ -0,0 +1,20 @@
+package cn.edu.bupt;
+
+class DBTest {
+
+    @org.junit.jupiter.api.Test
+    void addStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateStudent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStudent() {
+    }
+}
