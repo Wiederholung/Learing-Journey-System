dc907bab1a9892e5d2db067fa973d03936965fa6
wiederholung <wiederholung@outlook.com>
2023-05-12 01:05:30 +0800

    测试修改学生信息功能

--- src/main/java/cn/edu/bupt/ModifyInfoApp.java
+++ src/main/java/cn/edu/bupt/ModifyInfoApp.java
@@ -4,49 +4,49 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
-public class StudentInfoApp extends JFrame {
-
-
-    public StudentInfoApp(String studentID) {
+public class ModifyInfoApp extends JFrame {
+    public ModifyInfoApp(String studentID) {
         // 创建顶部标题
-        JLabel titleLabel = new JLabel("Your Status Information", SwingConstants.CENTER);
+        JLabel titleLabel = new JLabel("Modify Your Status Information", SwingConstants.CENTER);
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
         titleLabel.setBorder(new EmptyBorder(10, 0, 10, 0));
 
-        // 创建表格
+        // 创建可以修改的表格
         String[][] data = loadStudentData(studentID);
         String[] COLUMN_NAMES = {"Info", "Value"};
         JTable table = new JTable(data, COLUMN_NAMES);
-        table.setEnabled(false);
+        table.setEnabled(true);
 
         // 创建返回按钮
         JButton returnButton = new JButton("Return");
         returnButton.addActionListener(e -> {
             // 关闭当前窗口
             dispose();
-
             // 创建并显示 LearningJourneyApp 窗口
             LearningJourneyApp app = new LearningJourneyApp(studentID);
             app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             app.setVisible(true);
         });
 
-        // 创建修改按钮
-        JButton modifyButton = new JButton("Modify");
-        modifyButton.addActionListener(e -> {
+        // 创建提交按钮
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(e -> {
+//            将修改后的信息写入db.json
+//            DB.modifyStudent(studentID, data);
+            JOptionPane.showMessageDialog(null, "Modify Successfully!");
+
             // 关闭当前窗口
             dispose();
-
-            // 创建并显示 ModifyInfoApp 窗口
-            ModifyInfoApp app = new ModifyInfoApp(studentID);
+            // 创建并显示 LearningJourneyApp 窗口
+            LearningJourneyApp app = new LearningJourneyApp(studentID);
             app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             app.setVisible(true);
         });
 
-        // 添加返回和修改按钮到底部
+        // 添加返回和提交按钮到底部
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(returnButton);
-        buttonPanel.add(modifyButton);
+        buttonPanel.add(submitButton);
 
         // 将组件添加到内容面板中
         JPanel contentPane = new JPanel(new BorderLayout());
@@ -60,8 +60,8 @@ public class StudentInfoApp extends JFrame {
         setTitle("Your Status Information");
         setSize(500, 400);
         setLocationRelativeTo(null);
+        pack();
     }
-
 
     private String[][] loadStudentData(String studentID) {
         String[][] data = new String[8][2];
