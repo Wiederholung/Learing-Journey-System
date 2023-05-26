533c478cd13725e5b73e0f8d3f2c1809f806bdfb
wiederholung <wiederholung@outlook.com>
2023-05-27 00:14:16 +0800

    update JavaDoc

---
+++ doc/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.metattri"},{"l":"com.metattri.app"},{"l":"com.metattri.dao"},{"l":"com.metattri.entity"}];updateSearchResults();
